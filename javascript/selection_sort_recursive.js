7f306d0bf1b335fbc079c544b42e69f0fb9d5c56
QuenMBar <Qbeast360@gmail.com>
2021-03-22 13:43:01 -0700

    Done.

--- javascript/selection_sort_recursive.js
+++ javascript/selection_sort_recursive.js
@@ -1,13 +1,22 @@
-function selectionSortRecursive(arr) {
-  // type your code here
+function selectionSortRecursive(arr, sorted = []) {
+    if (arr.length === 0) {
+        return sorted;
+    }
+    const min = Math.min(...arr);
+    const idx = arr.indexOf(min);
+
+    sorted.push(min);
+    arr.splice(idx, 1);
+
+    return selectionSortRecursive(arr, sorted);
 }
 
 if (require.main === module) {
-  // add your own tests in here
-  console.log("Expecting: [-1, 2, 3, 5]");
-  console.log("=>", selectionSortRecursive([3, -1, 5, 2]));
+    // add your own tests in here
+    console.log("Expecting: [-1, 2, 3, 5]");
+    console.log("=>", selectionSortRecursive([3, -1, 5, 2]));
 
-  console.log("");
+    console.log("");
 }
 
 module.exports = selectionSortRecursive;
